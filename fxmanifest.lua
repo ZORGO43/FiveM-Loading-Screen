3fb3d20730bae672c8e3e7362bf8f31a195a13f4
Ajmal Rasheed <80527442+ZORGO43@users.noreply.github.com>
2024-04-01 05:53:17 -0700

    Added Files

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Zorgo-Loading-Screen'
+version '1.2.0'
+
+
+files {
+  'z-Ned/**',
+  'Z-Loading.html'
+}
+
+loadscreen {
+  'Z-Loading.html'
+}
+
+loadscreen_cursor 'yes'
+loadscreen_manual_shutdown 'yes'
+
+lua54 'yes'
